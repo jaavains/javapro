94d4239e8c1d0b1580403b644c833bb52f9b4115
avinash <avinxt@gmail.com>
2022-05-27 11:23:45 +0530

    day1

--- src/main/java/day1.java
+++ src/main/java/day1.java
@@ -1,7 +1,7 @@
 public class day1 {
 
     public static void main(String[] args) {
-        System.out.println("Avina  sh   hello! !!");
+        System.out.println("Avinash   hello! !!");
     }
 }
 
