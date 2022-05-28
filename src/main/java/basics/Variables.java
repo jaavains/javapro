c01fb5fa1fc423b56a081e60a49278fb014a2d64
avinash <avinxt@gmail.com>
2022-05-28 18:01:05 +0530

    (int/int) * float = int *float

--- src/main/java/basics/Variables.java
+++ src/main/java/basics/Variables.java
@@ -1,17 +1,13 @@
 package basics;
-
 import basics.dtypes.Student;
-
 public class Variables {
     static int ss; // class variables    : specific to the class & global across all classes. s
     int a ;       // instance variables. : specific to object instance
-
     long l1 = 100;
     int i1 = (int) l1;   // narrowing type conversion. we need to explicitly cast.
                         // compiler doesnt know that its smaller ,as its known at runtime.
                         // so casting should be done.
     long l2 = i1 ; // auto casting .
-
     double d1 = (2 + 3 ) / 2;  // outputs an int. 2.0
     double d2 = (double )(2 + 3 ) / 2; // outputs a double. 2.5
 
