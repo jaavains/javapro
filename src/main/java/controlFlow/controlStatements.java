ebd31f698086d58c910e8a55ee0abb4f551f50b8
avinash <avinxt@gmail.com>
2022-05-30 21:36:51 +0530

    switch +case : single condition , int , string , enum only.

--- src/main/java/controlFlow/controlStatements.java
+++ src/main/java/controlFlow/controlStatements.java
@@ -14,9 +14,9 @@ public class controlStatements {
         int b;
 
 //              switch -- case .
+ // Only works with 'one' condition to test & condition being a equality one. If not use if-else.
 // expression can be only of : Integer , String , enum
 // a compile time constant can be used in case. use final its outside scope of switch-case.
-// Only works with one condition to test & condition being a equality one. If not use if-else.
 //case label cannot be null.
 // It  must be a constant variable ' within scope of switch'. Even method arguments are outside of case scope.
 //  we can use a final int a = 0 , which should be a compile time constant.
