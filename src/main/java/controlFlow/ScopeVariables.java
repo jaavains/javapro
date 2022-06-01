4788609e36f4bae8768d555c7b81f6de751c1730
avinash <avinxt@gmail.com>
2022-06-01 22:15:06 +0530

    scopes

---
+++ src/main/java/controlFlow/ScopeVariables.java
@@ -0,0 +1,7 @@
+package controlFlow;
+
+public class ScopeVariables {
+//     Local variable is accessible from initialization line till end of block.
+//     A local variable can hide class level variable.  If we want to use class level use 'this' reference.
+
+}
