81f0535088df7606016c16d005a1eacd5e50e9af
avinash <avinxt@gmail.com>
2022-06-02 20:24:44 +0530

    stringBuilders.append("a")

---
+++ src/com/javapro/packageStrings/stringBuilders.java
@@ -0,0 +1,18 @@
+package packageStrings;
+
+import java.sql.SQLOutput;
+
+public class stringBuilders {
+//     String builder is mutable.
+//     Is not synchronized , so simultanious modifications cannot happen .
+
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(":").append("avin").append(12.0).append("ash");
+        System.out.println(sb);
+        System.out.println(sb.reverse()); // reverse
+        System.out.println(sb.reverse().delete(5,9));
+
+    }
+}
+// StringBuffer --> older api , but synchronized , so it slows down the execution as any time 1 thread modifies the object.
