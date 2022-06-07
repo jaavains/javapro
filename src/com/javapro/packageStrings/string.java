7f99b7bf6592ef146797e11b48c7a027dc6ba771
avinash <avinxt@gmail.com>
2022-06-07 10:39:59 +0530

    utf-16 char array

--- src/com/javapro/packageStrings/string.java
+++ src/com/javapro/packageStrings/string.java
@@ -4,13 +4,11 @@ public class string {
     public static void main(String[] args) {
 
         String s1 = new String("hello") ; //  "hello" is an object reference of String literal which is a string object
-        String s2 = new String(new char[]{'l','o'}); // internalyy strings is immutable sequence of char arrays.
+        String s2 = new String(new char[]{'l','o'}); // internally strings is immutable sequence of char arrays. using utf-16
 
 //         Recommended way for optimizations is via string literal below that saves strings in String pool.
 //         Same string in String pool will have same objectID saving space .
         String sbetterApproach = "avinash"; // "avinash" is string literal ,. This works for strings & box primitives.
-
-
         System.out.println(s1.isEmpty()); // is string empty ?
         System.out.println(s1.equalsIgnoreCase(s2)); // true
         System.out.println(s1.compareTo(s2));  // Compares lexicographically. | 0 when matching
