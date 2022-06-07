476a64c9e0eb50599f08c89692d40b36e198619e
avinash <avinxt@gmail.com>
2022-06-07 10:40:29 +0530

    done

--- src/com/javapro/temp/temp.java
+++ src/com/javapro/temp/temp.java
@@ -2,6 +2,12 @@ package temp;
 
 public class temp {
     public static void main(String[] args) {
-
+        for(int i=0;i<10;i++){
+            System.out.println(i);
+            if (i==5){
+                System.out.println("here");
+                break;
+            }
+        }
     }
 }
